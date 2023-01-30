3b3fe927c4754a88ee940aa14d0ba342b331d665
bhang <bhang@DESKTOP-QLRD6VD>
2023-01-30 14:41:39 +0530

    commit1

---
+++ Nimish/src/abc/eg1.java
@@ -0,0 +1,32 @@
+package abc;
+
+public class eg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("");
+
+//		int value1 =9/2;
+//		float value2 =101/61;
+//		double value3 =10d/6d;
+//		float value4 =5/2.5f;
+//	
+//		System.out.println("value 1 is :" + value1);
+//		System.out.println(4"value 2 is :" + value2);
+//		System.out.println("value 3 is :" + value3);
+//		System.out.println("value 4 is :" + value4);
+//		
+//		int marks = 512;
+//		float percentage = marks* 0.46f;
+//		System.out.println("Percentage \"is\" :" + percentage);
+		
+		int $num= 20;
+		System.out.println("Value of Numbefr is :" + $num);
+		
+		String Stuname = "Nimish";
+		System.out.println("Value is : " + Stuname);
+		
+		
+	}
+	
+}
